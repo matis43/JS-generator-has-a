65628bc2ac164ec69655334c99627b92a5c17c06
TraszZZZ <54902653+matis43@users.noreply.github.com>
2022-04-19 23:46:01 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -1,18 +1,41 @@
 var iloscZn=8;
 var iloscCyfr=0;
+var iloscMaleL=0;
+var iloscWolnychZn=0;
+var iloscDuzeL=0;
+var iloscSpec=0;
 
 function iloscZnakow(){
     iloscZn=document.getElementById("iloscZnakow").value;
-    document.getElementById("iloscCyfr").max=iloscZn;
-    document.getElementById("iloscMaleL").max=iloscZn;    
+    
+    ustawWolne();
 }
 
 function wybCyfr(){
     iloscCyfr=document.getElementById("iloscCyfr").value;
     document.getElementById("wybCyfr").value=iloscCyfr;
+    ustawWolne();
 }
-
 function wybMaleL(){
-    iloscCyfr=document.getElementById("iloscMaleL").value;
-    document.getElementById("wybMaleL").value=iloscCyfr;
+    iloscMaleL=document.getElementById("iloscMaleL").value;
+    document.getElementById("wybMaleL").value=iloscMaleL;
+    ustawWolne();
+}
+function ustawWolne(){
+    iloscWolnychZn=iloscZn-iloscCyfr-iloscMaleL-iloscDuzeL-iloscSpec;
+    document.getElementById("wynik").value=iloscWolnychZn;
+    document.getElementById("iloscCyfr").max=+iloscCyfr +iloscWolnychZn;
+    document.getElementById("iloscMaleL").max=+iloscMaleL +iloscWolnychZn;
+    document.getElementById("iloscDuzeL").max=+iloscDuzeL +iloscWolnychZn;
+    document.getElementById("iloscSpec").max=+iloscSpec +iloscWolnychZn;
+}
+function wybDuzeL(){
+    iloscDuzeL=document.getElementById("iloscDuzeL").value;
+    document.getElementById("wybDuzeL").value=iloscDuzeL;
+    ustawWolne();
+}
+function wybSpec(){
+    iloscSpec=document.getElementById("iloscSpec").value;
+    document.getElementById("wybSpec").value=iloscSpec;
+    ustawWolne();
 }
